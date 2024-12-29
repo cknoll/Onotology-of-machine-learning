6c46fd71589e9099843ce5411ed06b36aa503d5f
Carsten Knoll <carsten.knoll@posteo.de>
2024-12-29 07:29:42 +0100

    feat (test_package.py): allow test-execution with `python -m unittest`
    This is desired for the ci-job

--- docs/explanations.md
+++ docs/explanations.md
@@ -8,5 +8,9 @@ The idea is that for every git-commit which contains such a change, there is som
 
 ## Reasons
 
-- I want to have an easy to overlook structure for the package-repo (with not so many files in the root dir).
-- There might be more test-files in the future.
+- the tests should be executable both by
+    - `pytest`
+        - I prefer and recommend this for manual testing (due to nicer output)
+    - `python -m unittest`
+        - this is used for the CI job (to avoid an unnecessary dependency and it seems to be faster)
+        - Note: the `unittest` package is part of Python standard-library while `pytest` is not.
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,21 +1,23 @@
-import pytest
 import pyirk as p
+import unittest
 from oml import I1001, I1002
 
-def test_subclass_relationship():
-    """
-    Checks that I1002-Supervised Learning is a subclass of I1001-Machine Learning
-    """
+class TestPackage01(unittest.TestCase):
 
-    relations_dict = I1002.get_relations()
-    subclass_statements = relations_dict.get("irk:/builtins#R3", [])
+    def test_subclass_relationship(self):
+        """
+        Checks that I1002-Supervised Learning is a subclass of I1001-Machine Learning
+        """
 
-    found_relationship = False
-    for stmt in subclass_statements:
-        if stmt.object == I1001["Machine Learning"]:
-            found_relationship = True
-            break
+        relations_dict = I1002.get_relations()
+        subclass_statements = relations_dict.get("irk:/builtins#R3", [])
 
-    assert found_relationship, (
-        "I1002 should be a subclass of I1001, but no matching Statement was found."
-    )
+        found_relationship = False
+        for stmt in subclass_statements:
+            if stmt.object == I1001["Machine Learning"]:
+                found_relationship = True
+                break
+
+        assert found_relationship, (
+            "I1002 should be a subclass of I1001, but no matching Statement was found."
+        )
